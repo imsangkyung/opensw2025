258bd5d200aedb7769829412ac6c223ba70d1b4d
Sangkyung Im <imsangkyung@gmail.com>
2025-11-06 15:00:41 +0900

    new foofoo and foofoofoo are defined

---
+++ src/func1.c
@@ -0,0 +1,17 @@
+#include "func1.h"
+
+void foo(void)
+{
+    printf("Hello foo! \n");
+}
+
+void foofoo(void)
+{
+    printf("Hello foo foo! \n");
+}
+
+void foofoofoo(void)
+{
+    printf("Hello foo foo foo! \n");
+}
+
